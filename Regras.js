80aa2da4c23969ebb09893db6a714343f65dc9b4
BRENO MATEUS FERNANDES <breno.mfernandes@senacsp.edu.br>
2019-08-08 20:38:36 -0300

    Conhecendo regras

---
+++ Regras.js
@@ -0,0 +1,30 @@
+// Exemplo para não se fazer 
+// let 1nome = ''; 
+
+// Exenplo para não se fazer
+// string 
+let nome = 'vinicius' 
+
+// Tipo inteiro 
+let idade = 20
+
+// Tipo Decimal 
+let peso = 55.25
+
+// Tipo boleano
+let visualizado = true;
+
+// Criar variavel constante
+const nome1 = 'vinicius';
+
+//Exibir informações 
+console.log ('Esta é no console.log');
+
+console.log ('Exibindo variáveis');
+console.log ('Nome: ' + nome)
+nome = 'José';
+console.log('Nome: ' + nome)
+console.log('Nome const;' + nome1) 
+
+console.log('Nome; ' + nome + 'idade:'+ idade)
+console.log(`Nome: ${nome} idade ${idade}`)
