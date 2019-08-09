5d03499e357faed9ae4185af94cb361f34e9baff
BRENO MATEUS FERNANDES <breno.mfernandes@senacsp.edu.br>
2019-08-08 22:10:48 -0300

    Lista

---
+++ lista.js
@@ -0,0 +1,18 @@
+// Amtigamente
+var listaAntiga = Array();
+
+// Novo
+let lista = []; 
+console.log(lista);
+
+//Adicionado um item
+lista.push('carvão')
+lista.push(10)
+lista.push('ceveja sem alcool')
+
+// Que todo array começa com zero
+console.log(lista[0]);
+
+// O pop sempre remove o ultimo item da lista
+console.log(lista.pop())
+console.log(lista);
